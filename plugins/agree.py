66d8fef00c4e202d46e39013ae66d3cf464c4342
ssook901 <61262951+ssook901@users.noreply.github.com>
2020-02-20 13:51:17 +0900

    Update agree.py

--- plugins/agree.py
+++ plugins/agree.py
@@ -10,6 +10,9 @@ class AgreePlugin(MachineBasePlugin):
             'ㅇㅇ',
             '조습니다',
             'ㅇㅇ 조습니다',
+            '헐 ㅇㅇ',
+            '너무 좋다',
+            '1)왜 그런지랑 2)뭐가 좋은지, 알려주시면 판단하기 좋을 것 같아요'
         ]
 
         response = random.choice(candidates)
